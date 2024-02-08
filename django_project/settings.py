b2fccea2543fba42327eb39f98550a78f3386ed8
Derrell Jones <70511199+derrellj@users.noreply.github.com>
2024-02-08 09:34:42 +0300

    changed db

--- django_project/settings.py
+++ django_project/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-    }
-}
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
+#  {
+#     "ENGINE": "django.db.backends.postgresql",
+#     "NAME": os.environ.get("DB_NAME"),
+#     "USER": os.environ.get("DB_USER"),
+#     "PASSWORD": os.environ.get("DB_PASSWORD"),
+#     "HOST": os.environ.get("DB_HOST"),
+#     "PORT": os.environ.get("DB_PORT"),
+# }
+
 
 # DATABASES = {
 #     "default": {
